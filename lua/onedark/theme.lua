b94481f5526fb8f20575e1bfe55b05cb2736b0b6
Travis Waelbroeck <twaelbroeck@gmail.com>
2021-12-10 23:47:43 -0600

    [UPD] Tweak colors & configuration
    - Use same background (black/transparent) for gutter
    - Style folds as comment-colored
    - Rework theme to be fewer colors (mainly removing red for variables)
    - Lighter color column
    - Brighter green

--- lua/onedark/theme.lua
+++ lua/onedark/theme.lua
@@ -34,7 +34,7 @@ function M.setup(config)
     -- TermCursorNC= { }, -- cursor in an unfocused terminal
     ErrorMsg = {fg = c.error}, -- error messages on the command line
     VertSplit = {fg = c.bg_visual}, -- the column separating vertically split windows
-    Folded = {fg = c.blue, bg = c.fg_gutter}, -- line used for closed folds
+    Folded = {fg = c.syntax.comment, style = config.comment_style}, -- line used for closed folds
     FoldColumn = {bg = c.bg, fg = c.fg_gutter}, -- 'foldcolumn'
     SignColumn = {bg = config.transparent and c.none or c.bg_linenumber, fg = c.fg_gutter}, -- column where |signs| are displayed
     SignColumnSB = {bg = c.bg_sidebar, fg = c.fg_gutter}, -- column where |signs| are displayed
